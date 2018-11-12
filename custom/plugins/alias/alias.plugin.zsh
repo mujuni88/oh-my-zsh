a4d9114ed2285b8e6452d2a62b25d8d4a10fe0c5
Joe Buza <joebm08@gmail.com>
2018-11-12 17:22:51 -0600

    Create alias.plugin.zsh

---
+++ custom/plugins/alias/alias.plugin.zsh
@@ -0,0 +1,32 @@
+# Project Paths
+alias Code="cd ~/Code/"
+alias ss="Code && cd schoolstatus/"
+alias pr="Code && cd projects"
+alias tuts="Code && cd tutorials"
+alias aliases="$ZSH/custom/plugins/alias/"
+alias dtf="$HOME/.dotfiles/"
+
+# Custom projects
+alias ssui="ss && cd ss-ui/"
+
+# File management
+alias ls="ls -al"
+alias df="df -h"
+alias rm="rm -i"
+alias cp="cp -i"
+
+# Vim
+# alias vi="nvim"
+# alias vim="nvim"
+
+# Tmux
+alias tmn="tmux new-session -s"
+alias tma="tmux attach -t"
+alias tml="tmux ls"
+alias tmk="tmux kill-session -t"
+alias tmd="tmux detach -a"
+alias tmka="killall tmux"
+
+# Python
+# alias python="python3"
+# alias py3="python3"
